323a22d6f5f789d760c51a356c29ce1f43b90395
Shejal <shejal270999@gmail.com>
2021-12-05 20:59:22 +0530

    Added some dice features

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,13 @@
 console.log("This is a JS file");
+
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+let img1 = "images/dice" + randomNumber1 + ".png";
+let img2 = "images/dice" + randomNumber2 + ".png";
+
+let dice1 = document.querySelectorAll("img")[0];
+let dice2 = document.querySelectorAll("img")[1];
+
+dice1.setAttribute("src",img1);
+dice2.setAttribute("src",img2);
