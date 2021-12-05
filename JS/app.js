1ff48dbffa84f7ff38f523992e607b0a4a76cb0d
Shejal <shejal270999@gmail.com>
2021-12-05 21:04:38 +0530

    Added the logic to decide which player wins

--- JS/app.js
+++ JS/app.js
@@ -11,3 +11,18 @@ let dice2 = document.querySelectorAll("img")[1];
 
 dice1.setAttribute("src",img1);
 dice2.setAttribute("src",img2);
+
+if(randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").innerHTML ="Player 1 Wins!";
+}
+
+else if(randomNumber1 < randomNumber2)
+{
+    document.querySelector("h1").innerHTML ="Player 2 Wins!";
+}
+
+else
+{
+    document.querySelector("h1").innerHTML ="Draw!";
+}
